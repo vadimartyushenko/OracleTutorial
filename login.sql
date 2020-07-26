e16b7ed32f57bf1ea2e53b1d81af5d14a4b522ad
Artyushenko Vadim <artushenkomail@mail.ru>
2020-07-27 00:18:19 +0700

    Update login.sql
    добавил коммент

--- login.sql
+++ login.sql
@@ -6,4 +6,5 @@ set long 5000;
 set linesize 100;
 set pagesize 9999;
 column plan_plus_exp format a80;
+--настройка строки приглашения на ввод команд SQL*Plus (строка приобретает вид имя_пользователя@идентификатор_подключения)
 set sqlprompt '&_user.@&_connect_identifier.>';
